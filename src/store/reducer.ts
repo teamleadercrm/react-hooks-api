9d64d75e522f14e9f2ccba16b1d7e4799257d893
Arnaud Weyts <arnaud.weyts@gmail.com>
2019-06-06 14:25:57 +0200

    :fire: remove the simple root reducer that just saved the data

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,28 +1,7 @@
-import { DeepReadonly } from 'utility-types';
-import { createReducer } from 'typesafe-actions';
+import { combineReducers } from 'redux';
 
-import { CACHE_QUERY_RESULT } from './constants';
-import generateQueryCacheKey from '../utils/generateQueryCacheKey';
-import { Response } from '../typings/API';
+import queries from './queries/reducer';
 
-import { RootAction } from './actions';
-
-export type State = DeepReadonly<{
-  queries: {
-    [key: string]: Response;
-  };
-}>;
-
-const INITIAL_STATE: State = {
-  queries: {},
-};
-
-const queries = createReducer<State, RootAction>(INITIAL_STATE).handleAction(
-  CACHE_QUERY_RESULT,
-  (state, { payload }) => {
-    const key = generateQueryCacheKey({ domain: payload.domain, action: payload.action, options: payload.options });
-    return { ...state, queries: { ...state.queries, [key]: payload.data } };
-  },
-);
-
-export default queries;
+export default combineReducers({
+  queries,
+});
