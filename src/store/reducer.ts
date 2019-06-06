8cf905ebe8dc3070951a2b5838563965718bea11
Arnaud Weyts <arnaud.weyts@gmail.com>
2019-06-06 15:39:53 +0200

    :sparkles: hook up the reducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import queries from './queries/reducer';
+import entities from './entities/reducer';
 
 export default combineReducers({
   queries,
+  entities,
 });
