2885294276ecf7285a69caa4b43afbdfcb3c2a32
Arnaud Weyts <arnaud.weyts@gmail.com>
2019-06-11 14:23:17 +0200

    :white_check_mark: test selectors

---
+++ src/store/queries/__tests__/selectors.spec.ts
@@ -0,0 +1,27 @@
+import { selectQuery, selectMetaFromQuery } from '../selectors';
+
+describe('queries selectors', () => {
+  const INITIAL_STATE = {
+    entities: {},
+    queries: {
+      uniqueKey: {
+        loading: false,
+        meta: {
+          matches: 2,
+        },
+      },
+    },
+  };
+
+  it('selects the correct query', () => {
+    const query = selectQuery(INITIAL_STATE, 'uniqueKey');
+
+    expect(query).toEqual({ loading: false, meta: { matches: 2 } });
+  });
+
+  it('selects the meta of the query', () => {
+    const meta = selectMetaFromQuery(INITIAL_STATE, 'uniqueKey');
+
+    expect(meta).toEqual({ matches: 2 });
+  });
+});
