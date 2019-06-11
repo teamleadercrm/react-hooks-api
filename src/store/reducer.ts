679c694c661d0ee9582f1f530430f83c608d7e98
Arnaud Weyts <arnaud.weyts@gmail.com>
2019-06-11 09:22:23 +0200

    :label: write a type for the root state

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
+import { DeepReadonly } from 'utility-types';
 
-import queries from './queries/reducer';
-import entities from './entities/reducer';
+import queries, { State as QueriesState } from './queries/reducer';
+import entities, { State as EntitiesState } from './entities/reducer';
+
+export type State = DeepReadonly<{
+  queries: QueriesState;
+  entities: EntitiesState;
+}>;
 
 export default combineReducers({
   queries,
