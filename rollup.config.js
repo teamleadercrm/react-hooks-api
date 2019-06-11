6cc496781a767e8a01eba1a030189a2f08620242
Arnaud Weyts <arnaud.weyts@gmail.com>
2019-06-11 10:31:43 +0200

    :wrench: lodash.module uses commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import typescriptPlugin from 'rollup-plugin-typescript2';
 import typescript from 'typescript';
+import commonjs from 'rollup-plugin-commonjs';
 
 import packageJSON from './package.json';
 
@@ -23,6 +24,9 @@ export default {
       customResolveOptions: {
         moduleDirectory: 'node_modules'
       }
+    }),
+    commonjs({
+      include: ['node_modules/lodash.get/index.js', 'node_modules/lodash.set/index.js'],
     }),
   ],
   external: ['react', 'react-dom', 'react-redux', '@teamleader/api']
