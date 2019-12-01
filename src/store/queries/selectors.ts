e6a852dcc638d8d9f27c07fe78b8e985ba7ba4dc
Arnaud Weyts <arnaud.weyts@gmail.com>
2019-12-01 16:34:53 +0100

    :sparkles: implement the selector

--- src/store/queries/selectors.ts
+++ src/store/queries/selectors.ts
@@ -4,6 +4,11 @@ export const selectQueryWithKey = (key: string) => (state: State) => {
   return state.queries[key];
 };
 
+export const selectLoadingFromQuery = (key: string) => (state: State) => {
+  const query = selectQueryWithKey(key)(state);
+  return query && query.loading || false;
+}
+
 export const selectMetaFromQuery = (key: string) => (state: State) => {
   const query = selectQueryWithKey(key)(state);
   return query && query.meta;
