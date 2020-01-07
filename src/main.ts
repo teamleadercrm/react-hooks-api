a62d825828367ae7c3483fc209364ccbb0fb972e
Arnaud Weyts <arnaud.weyts@gmail.com>
2020-01-07 16:34:45 +0100

    :sparkles: export the decodeQueryCacheKey and generateQueryCacheKey functions
    this way people can access certain queries

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import useQuery, { queries } from './useQuery';
 import Provider from './Provider';
+import decodeQueryCacheKey from './utils/decodeQueryCacheKey';
+import generateQueryCacheKey from './utils/generateQueryCacheKey';
 
-export { queries, useQuery, Provider };
+export { queries, useQuery, Provider, decodeQueryCacheKey, generateQueryCacheKey };
 
 export default {
   queries,
   useQuery,
   Provider,
+  decodeQueryCacheKey,
+  generateQueryCacheKey
 }
