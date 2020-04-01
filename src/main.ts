deb619bf7b178b4d9d9945dcc2887dfeb7b94d8a
Arnaud Weyts <arnaud.weyts@gmail.com>
2020-04-01 15:27:56 +0200

    :sparkles: expose the store's state by exporting the useSelector hook

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,22 @@ import useQuery, { queries } from './useQuery';
 import Provider from './Provider';
 import decodeQueryCacheKey from './utils/decodeQueryCacheKey';
 import generateQueryCacheKey from './utils/generateQueryCacheKey';
+import { useSelector } from './store/CustomReduxContext';
 
-export { queries, useQuery, Provider, decodeQueryCacheKey, generateQueryCacheKey };
+export {
+  queries,
+  useQuery,
+  Provider,
+  decodeQueryCacheKey,
+  generateQueryCacheKey,
+  useSelector as useReactHooksAPISelector,
+};
 
 export default {
   queries,
   useQuery,
   Provider,
   decodeQueryCacheKey,
-  generateQueryCacheKey
-}
+  generateQueryCacheKey,
+  useReactHooksAPISelector: useSelector,
+};
