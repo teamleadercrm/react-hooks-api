651af819676d6a9d77f54693262104d910888ef0
Arnaud Weyts <arnaud.weyts@gmail.com>
2020-04-01 14:43:52 +0200

    :sparkles: create new selectors to select a single entity or an array of entities

--- src/store/entities/selectors.ts
+++ src/store/entities/selectors.ts
@@ -1,3 +1,35 @@
+import { createSelector, createSelectorCreator } from 'reselect';
 import { State } from '../reducer';
+import { selectIdsFromQuery, selectDomainNameFromQuery } from '../queries/selectors';
+import { memoizeWithResultArrayEntryShallowCheck } from './memoizeWithResultArrayEntryShallowCheck';
+
+/**
+ * Creates a selector with array memoization support
+ * This way we check for the equality of each entry in an array
+ * instead of the array itself, which prevents re-renders
+ * https://github.com/reduxjs/reselect/issues/74#issuecomment-271762561
+ */
+// @TODO remove once the typing for createSelectorCreator is fixed
+// @ts-ignore
+const createSelectorWithResultArrayMemoization = createSelectorCreator(memoizeWithResultArrayEntryShallowCheck);
 
 export const selectEntities = (state: State) => state.entities;
+
+export const selectDomainFromQuery = createSelector(
+  selectDomainNameFromQuery,
+  selectEntities,
+  (domainName, entities) => entities[domainName],
+);
+
+export const selectEntitiesFromQueryFactory = () =>
+  createSelectorWithResultArrayMemoization(selectIdsFromQuery, selectDomainFromQuery, (ids, domain) => {
+    if (!ids) {
+      return null;
+    }
+
+    if (Array.isArray(ids)) {
+      return ids.map((id) => domain[id]);
+    }
+
+    return domain[ids];
+  });
