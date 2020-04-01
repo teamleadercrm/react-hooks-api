54362cb762058e06bb6ea36144f021792099f290
Arnaud Weyts <arnaud.weyts@gmail.com>
2020-04-01 14:22:58 +0200

    :sparkles: write a custom memoization function that shallowly compares the
    entries of resulting arrays

---
+++ src/store/entities/memoizeWithResultArrayEntryShallowCheck.ts
@@ -0,0 +1,34 @@
+const shallow = (previous: any, next: any) => {
+  return previous === next;
+};
+
+const shallowOnArray = (previous: any, next: any) => {
+  if (Array.isArray(previous) && Array.isArray(next)) {
+    return previous.every((item, index) => shallow(item, next[index]));
+  }
+
+  /**
+   * return false because we only want to support
+   * a shallow check on array entries, if the result
+   * is not an array (shallow / different object)
+   * we should return a new result
+   */
+  return false;
+};
+
+export const memoizeWithResultArrayEntryShallowCheck = (func: (...args: any[]) => any) => {
+  let lastArgs = null;
+  let lastResult = null;
+  return (...args) => {
+    if (
+      lastArgs !== null &&
+      lastArgs.length === args.length &&
+      args.every((value, index) => shallow(value, lastArgs[index]))
+    ) {
+      return lastResult;
+    }
+    lastArgs = args;
+    const result = func(...args);
+    return shallowOnArray(lastResult, result) ? lastResult : (lastResult = result);
+  };
+};
