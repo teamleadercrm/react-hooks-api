c1db0bebc83f831ea87fc710df67d8bdfafdc401
Arnaud Weyts <arnaud.weyts@gmail.com>
2020-04-01 15:48:54 +0200

    :sparkles: expose the selectors in the package

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import Provider from './Provider';
 import decodeQueryCacheKey from './utils/decodeQueryCacheKey';
 import generateQueryCacheKey from './utils/generateQueryCacheKey';
 import { useSelector } from './store/CustomReduxContext';
+import { selectEntityByDomainAndIdFactory, selectEntitiesByDomainAndIdsFactory } from './store/entities/selectors';
 
 export {
   queries,
@@ -11,6 +12,8 @@ export {
   decodeQueryCacheKey,
   generateQueryCacheKey,
   useSelector as useReactHooksAPISelector,
+  selectEntityByDomainAndIdFactory,
+  selectEntitiesByDomainAndIdsFactory,
 };
 
 export default {
@@ -20,4 +23,6 @@ export default {
   decodeQueryCacheKey,
   generateQueryCacheKey,
   useReactHooksAPISelector: useSelector,
+  selectEntityByDomainAndIdFactory,
+  selectEntitiesByDomainAndIdsFactory,
 };
