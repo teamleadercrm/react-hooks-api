e8cf8ee1dd510e9db937390f710610f9301ba2db
Arnaud Weyts <arnaud.weyts@gmail.com>
2020-04-01 12:06:52 +0200

    :white_check_mark: add missing test for selectEntities

--- src/store/entities/__tests__/selectors.spec.ts
+++ src/store/entities/__tests__/selectors.spec.ts
@@ -1,3 +1,16 @@
 import { selectEntities } from '../selectors';
 
-describe('Entities selectors', () => {});
+describe('Entities selectors', () => {
+  describe('selectEntities', () => {
+    it('selects the entities state', () => {
+      const state = {
+        queries: {},
+        entities: {
+          projects: {},
+        },
+      };
+
+      expect(selectEntities(state)).toEqual({ projects: {} });
+    });
+  });
+});
